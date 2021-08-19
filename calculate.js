3663795bf120469777057e1a6b8e707c9a41c621
hssnfarooq <hhhassanhafeez91@gmail.com>
2021-08-19 23:40:43 +0500

    done

--- calculate.js
+++ calculate.js
@@ -1,16 +1,21 @@
 var form = document.getElementById("domesticParcel");
 var fromData = {};
 var toData = {};
+var parcelInfo = {};
 var small = false;
 var medium = false;
 var large = false;
-var basePrice = 4.5;
+var basePrice = 65;
 var price = 0;
 var totalPrice = 0;
 form.addEventListener("submit", () => {
   //event.preventDefault();
   var parcel = Number(document.getElementById("numParcels").value);
+  var parcelItem = document.getElementById("domesticRestrictedItems");
+  parcelItem = parcelItem.options[parcelItem.selectedIndex].text;
+  console.log(parcelItem);
   for (let i = 1; i <= parcel; i++) {
+    var weight = document.getElementById(`${i}-weight`).value;
     var width = document.getElementById(`${i}-width`).value;
     var height = document.getElementById(`${i}-height`).value;
     var length = document.getElementById(`${i}-length`).value;
@@ -25,9 +30,16 @@ form.addEventListener("submit", () => {
       large = true;
     }
     if (i > 1) {
-      price += 1.5;
+      price += 20;
     }
     calculateParcelPrice();
+    parcelInfo = {
+      weight: weight,
+      width: width,
+      height: height,
+      length: length,
+    };
+    localStorage.setItem(`Parcel${i}`, JSON.stringify(parcelInfo));
   }
 
   totalPrice = basePrice + price;
@@ -44,20 +56,22 @@ form.addEventListener("submit", () => {
   };
   localStorage.setItem("fromData", JSON.stringify(fromData));
   localStorage.setItem("toData", JSON.stringify(toData));
+  localStorage.setItem("parcel", parcel);
+  localStorage.setItem("parcelItem", parcelItem);
   localStorage.setItem("totalPrice", totalPrice);
 });
 
 function calculateParcelPrice() {
   if (small) {
-    price += 2;
+    price += 35;
     small = false;
   }
   if (medium) {
-    price += 3.5;
+    price += 50;
     medium = false;
   }
   if (large) {
-    price += 6;
+    price += 90;
     large = false;
   }
 }
@@ -66,7 +80,7 @@ function parcelChange(num) {
   var div = document.getElementById("weights");
   div.innerHTML = `
   
-  <input name="ItemDimensions[0].Weight" required id="weight" placeholder="Weight (kg)" type="number" min="0.1" step="0.1" max="15" class="half" />
+  <input name="ItemDimensions[0].Weight" required id="1-weight" placeholder="Weight (kg)" type="number" min="0.1" step="0.1" max="15" class="half" />
 
   <input name="ItemDimensions[0].Length" required id="1-length" placeholder="Length (cm)" type="number" min="10" step="1" max="120" class="half" />
 
@@ -77,7 +91,9 @@ function parcelChange(num) {
     var newDiv = document.createElement("div");
     newDiv.innerHTML = `<h4 style="font-size: 12px">Parcel ${i + 1}</h4>
   
-  <input name="ItemDimensions[0].Weight" required id="weight" placeholder="Weight (kg)" type="number" min="0.1" step="0.1" max="15" class="half" />
+  <input name="ItemDimensions[0].Weight" required id="${
+    i + 1
+  }-weight" placeholder="Weight (kg)" type="number" min="0.1" step="0.1" max="15" class="half" />
 
   <input name="ItemDimensions[0].Length" required id="${
     i + 1
